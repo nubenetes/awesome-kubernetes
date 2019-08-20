0e39e540a6ac8266348b92466aa7b344078772e7
Inaki <inafev@users.noreply.github.com>
2019-08-20 17:33:39 +0200

    Create istio.sh

---
+++ docs/istio.sh
@@ -0,0 +1,30 @@
+# Istio - Service Mesh
+- [blog.openshift.com: How to Explain Service Mesh in Plain English](https://blog.openshift.com/from-the-enterprisersproject-how-to-explain-service-mesh-in-plain-english/)
+- [Istio.io](https://istio.io/)
+- [github.com: Istio](https://github.com/istio/istio)
+- [Red Hat Developer: Istio Service Mesh](https://developers.redhat.com/topics/service-mesh/)
+- [github: redhat-developer-demos Istio Tutorial for Java Microservices](https://github.com/redhat-developer-demos/istio-tutorial)
+- [karlstoney.com: Istio 503's with UC's and TCP Fun Times](https://karlstoney.com/2019/05/31/istio-503s-ucs-and-tcp-fun-times/)
+
+## Maistra Istio
+- [Maistra.io](https://maistra.io)
+- [github.com: Maistra Istio](https://github.com/maistra/istio)
+- [Installing on OKD/OCP](https://maistra.io/docs/getting_started/install/)
+
+## Kiali project, observability for the Istio service mesh
+- [kiali.io](https://www.kiali.io/)
+- [github.com: kiali](https://github.com/kiali/kiali)
+- [medium.com: kiali project](https://medium.com/kialiproject)
+- [itnext.io: Find issues in your Istio mesh with Kiali](https://itnext.io/find-issues-in-your-istio-mesh-with-kiali-89d37d5e1fb1)
+
+## Jaeger tracing. Open source, end-to-end distributed tracing
+- Monitor and troubleshoot transactions in complex distributed systems
+- [jaegertracing.io](https://www.jaegertracing.io/)
+
+## Envoy micro proxy
+- [envoyproxy.io](https://www.envoyproxy.io/)
+- [getenvoy.io](https://www.getenvoy.io/)
+
+## AWS App Mesh
+- [aws.amazon.com/app-mesh](https://aws.amazon.com/app-mesh/)
+- [allthingsdistributed.com: Redefining application communications with AWS App Mesh](https://www.allthingsdistributed.com/2019/03/redefining-application-communications-with-aws-app-mesh.html)
